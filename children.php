7bdf5b1b87aea8cd01755770f8f308cc69078df2
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-12-17 12:36:47 +0200

    tgd r izmainas children.php

--- children.php
+++ children.php
@@ -2,6 +2,14 @@
 
 require "Database.php";
 
+$db = new Database($config["database"]);
+$kids = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);
 
+// Ar foreach izvadīt content
+echo "<ul>";
+foreach ($kids as $kid) {
+    echo "<li>" . $kid["firstname, middlename, surname, age"] . "</li>";
+};
+echo "</ul>";
 
 ?>
--- gifts.php
+++ gifts.php
@@ -2,14 +2,4 @@
 
 require "Database.php";
 
-$db = new Database($config["database"]);
-$kids = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);
-
-// Ar foreach izvadīt content
-echo "<ul>";
-foreach ($kids as $kid) {
-    echo "<li>" . $kid["firstname, middlename, surname, age"] . "</li>";
-};
-echo "</ul>";
-
 ?>
