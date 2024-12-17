edab922049192cae0ca81ff9bb544ed117c9a1c0
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-12-17 12:47:43 +0200

    papildināju children.php failu

--- children.php
+++ children.php
@@ -1,14 +1,25 @@
 <?php
 
+echo    "<style>
+            body {
+                background-color: #368BC1;
+                color: white;
+                font-size: 20px;
+                font-family: Consolas;
+            }
+        </style>";
+
 require "Database.php";
 
+$config = require ("config.php");
+
 $db = new Database($config["database"]);
-$kids = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);
+$children = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);
 
 // Ar foreach izvadīt content
 echo "<ul>";
-foreach ($kids as $kid) {
-    echo "<li>" . $kid["firstname, middlename, surname, age"] . "</li>";
+foreach ($children as $child) {
+    echo "<li>" . $child["firstname"] . " " . $child["middlename"] . " " . $child["surname"] . " - " . $child["age"] . "</li>";
 };
 echo "</ul>";
 
