e6e11e8a42e1c7167245054986885a21458c10e0
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-12-17 13:07:36 +0200

    izmaiņas children.php failā un gifts.php failā

--- gifts.php
+++ gifts.php
@@ -1,5 +1,26 @@
 <?php
 
+echo    "<style>
+            body {
+                background-color: #368BC1;
+                color: white;
+                font-size: 20px;
+                font-family: Consolas;
+            }
+        </style>";
+
 require "Database.php";
+
+$config = require ("config.php");
+
+$db = new Database($config["database"]);
+$gifts = $db->query("SELECT * FROM gifts")->fetchAll(PDO::FETCH_ASSOC);
+
+// Ar foreach izvadīt content
+echo "<ol>";
+foreach ($gifts as $gift) {
+    echo "<li>" . $gift["name"] . " - " . $gift["count_available"] . "</li>";
+};
+echo "</ol>";
 
 ?>
