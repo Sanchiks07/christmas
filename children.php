ecf3f3206e99117311b791dd4256eac3dead4cf7
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2024-12-17 13:57:30 +0200

    dažas izmaiņas pie children un papildinu css

--- children.php
+++ children.php
@@ -1,26 +1,22 @@
 <?php
 
-echo    "<style>
-            body {
-                background-color: #368BC1;
-                color: white;
-                font-size: 20px;
-                font-family: Consolas;
-            }
-        </style>";
-
+echo "<link rel='stylesheet' href='styles.css'>";
 require "Database.php";
-
 $config = require ("config.php");
 
 $db = new Database($config["database"]);
 $children = $db->query("SELECT * FROM children")->fetchAll(PDO::FETCH_ASSOC);
 
-// Ar foreach izvadīt content
-echo "<ul>";
+// Card container
+echo "<div class='card-container'>";
+
+// Display data in card format
 foreach ($children as $child) {
-    echo "<li>" . $child["firstname"] . " " . $child["middlename"] . " " . $child["surname"] . " - " . $child["age"] . "</li>";
-};
-echo "</ul>";
+    echo "<div class='card'>";
+    echo "<h3>" .$child["firstname"] . " " . $child["middlename"] . " " . $child["surname"] . " - " . $child["age"] . "</h3>";
+    echo "</div>";
+}
+
+echo "</div>"; // Close card container
 
 ?>
